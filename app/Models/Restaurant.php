13d6071601a536f55b42a7de06e65ba21ed0c2c8
Italo André <italoaaoliveira@gmail.com>
2025-01-19 16:18:47 -0300

    feat: adds Restaurant model, factory and migration

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Restaurant extends Model
+{
+    /** @use HasFactory<\Database\Factories\RestaurantFactory> */
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['external_id', 'name', 'slug', 'address'];
+    public $timestamps = true;
+}
